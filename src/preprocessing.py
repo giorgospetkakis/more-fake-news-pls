4a596b12d21bb363dd02906e5a5a8a78d106f564
Giorgos Petkakis <giorgospetkakis@users.noreply.github.com>
2020-05-21 02:19:10 +0200

    Create data processing script and processed data

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,21 +2,34 @@
 ## Sara, Flora, Giorgos
 
 import data
+from utils import go_to_project_root
 import numpy as np
 import pandas as pd
 
-filepath = '/data/processed/'
+filepath = 'data/processed/'
 
-def convert_to_df(authors):
+def convert_to_df(authors, export=False):
     '''
     Converts the given authors to a pandas DataFrame.
     Parameters:
         authors (Author dict):
             The authors to be converted.
+        export
     Returns:
         pandas DataFrame
     '''
-    table = np.zeros((len(list(authors)), 20))
-    df = pd.DataFrame(table)
+    # Can't not hard-code this
+    # Create table, fill table, convert to dataframe, name columns, return
+    table = np.zeros((len(list(authors.values())), 6))
+
+    for i, a in enumerate(list(authors.values())):
+        table[i] = [a.max_similar, a.min_similar, a.mean_similar, a.number_identical, a.most_common_ner_score, a.truth]
+
+    df = pd.DataFrame(table, columns=["max_similar", "min_similar", "mean_similar", "number_identical", "most_common_ner_score", "truth"])
+
+    # Enable to export to file
+    if export:
+        go_to_project_root()
+        df.to_csv(filepath+"processed.csv")
 
     return df
