b4a33c84786b2010268973bbbcfd78b7940b35d1
Giorgos Petkakis <giorgospetkakis@users.noreply.github.com>
2020-06-08 21:38:00 +0200

    Clean up code

--- src/modelling.py
+++ src/modelling.py
@@ -1,29 +1,32 @@
-from utils import go_to_project_root
-from scipy.stats import mode
-import data
 import os
-import pandas as pd
-import numpy as np
-from sklearn.decomposition import PCA
-from sklearn import svm
-from sklearn.metrics import balanced_accuracy_score, confusion_matrix, accuracy_score
-from sklearn.model_selection import KFold, train_test_split
-from matplotlib import pyplot as plt
 
-from sklearn.neural_network import MLPClassifier, BernoulliRBM
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, AdaBoostRegressor, GradientBoostingClassifier, ExtraTreesClassifier, GradientBoostingClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import GaussianNB, MultinomialNB
-import sklearn.svm
 import keras
-from sklearn.feature_selection import RFE
+import numpy as np
+import pandas as pd
+import sklearn.svm
+from keras.layers import LSTM, Dense
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
+from matplotlib import pyplot as plt
+from scipy.stats import mode
+from sklearn import svm
+from sklearn.decomposition import PCA
+from sklearn.ensemble import (
+    AdaBoostClassifier, AdaBoostRegressor, ExtraTreesClassifier,
+    GradientBoostingClassifier, RandomForestClassifier)
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (accuracy_score, balanced_accuracy_score,
+                             confusion_matrix, mean_squared_error)
+from sklearn.model_selection import KFold, train_test_split
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.neural_network import BernoulliRBM, MLPClassifier
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
+from sklearn.tree import DecisionTreeClassifier
+
+import data
 import xgboost as xgb
+from utils import go_to_project_root
+
 
 def classify(classifier, xtrain, ytrain, xtest, ytest):
     classifier.fit(xtrain, ytrain)
@@ -31,10 +34,10 @@ def classify(classifier, xtrain, ytrain, xtest, ytest):
     pred = predict(classifier, xtest)
     return pred
 
-def feature_elim(classifier, xtrain, ytrain):
-    rfe = RFE(estimator=classifier, n_features_to_select=n_features, step=10)
-    rfe.fit(xtrain, ytrain)
-    return rfe.ranking_
+# def feature_elim(classifier, xtrain, ytrain):
+#     rfe = RFE(estimator=classifier, n_features_to_select=n_features, step=10)
+#     rfe.fit(xtrain, ytrain)
+#     return rfe.ranking_
 
 def get_new_xtests(path): 
     path = data_root[:-1] + "s/" + path
@@ -58,37 +61,34 @@ def read_data(_path):
     for i, x in enumerate(xtest):
         xtest[i][:,[2, 3, 4, 5]] = mean_xt[i]
 
-    return xtrain, ytrain, xtest, ytest
+    return xtrain, ytrain.ravel(), xtest, ytest.ravel()
 
 def predict(classifier, xtest):
     majority_vote_preds = []
     for x in xtest:
-        x = x[:, _del]
         majority_vote_preds += [np.sum(classifier.predict(x).astype(int)) > 1]
     return majority_vote_preds
 
-
-go_to_project_root()
-data_root = "data/processed/800/"
-datasets = [read_data(f"K{k+1}/") for k in range(3)]
-
+# Hardcode all the different feature sets
 feature_sets = {
     "lexical": [0, 1],
     "semantic": [2, 3, 4, 5],
     "clusters": [6, 7],
     "nonling":  list(range(8, 30)),
-    "pos": [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47],
+    "pos": [30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47],
     "emotion": [47, 48, 49, 50, 51, 52, 53, 54, 55, 56],
     "embeddings": list(range(57, 357))
 }
+
+go_to_project_root()
+data_root = "data/processed/800/"
+datasets = [read_data(f"K{k+1}/") for k in range(3)]
 
 mean = 0
 for i in range(3):
     xtrain, ytrain, xtest, ytest = datasets[i]
     c = GradientBoostingClassifier()
-    # pred = classify(c, np.delete(xtrain, _del, axis=1), ytrain, xtest, ytest)
-    classify(c, xtrain[:, _del], ytrain, xtest, ytest)
+    pred = classify(c, xtrain, ytrain, xtest, ytest)
     acc = balanced_accuracy_score(ytest, pred)
     mean += acc
-print(mean / 3)
-
+print(f"Accuracy: {mean / 3}")
